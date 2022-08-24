3399691e8f097c959067960278b50c433061c0f3
Salah <Salah@LAPTOP-H2RKU42R>
2022-08-24 20:22:13 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+using System.Xml.Serialization;
+
+
+namespace this_is_for_xml_testing
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        const string filepath = "test2.xml";
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            DeserialzieXmlFiletoList();
+
+        }
+
+        
+        private void DeserialzieXmlFiletoList()
+        {
+            try
+            {
+                var xmlSerializer = new XmlSerializer(typeof(List<burgers>));
+                using (var reader = new StreamReader(filepath))
+                {
+                    var burgers = (List<burgers>)xmlSerializer.Deserialize(reader);
+                    DataGrid1.ItemsSource = burgers;
+                }
+            }
+
+            catch(FileNotFoundException e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            catch(Exception z)
+            {
+                MessageBox.Show(z.Message);
+            }
+        }
+
+    }
+}
---
+++ burgers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Xml.Serialization;
+
+namespace this_is_for_xml_testing
+{
+	[Serializable]
+	public class burgers
+    {
+        public string Name { get; set; }
+        public string Weight { get; set; }
+        public string Price { get; set; }
+
+    }
+}
